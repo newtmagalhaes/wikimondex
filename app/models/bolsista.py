0c81b19c86a5eec51c752dd0b1f778d70cda19d6
franshenrique <39673725+franshenrique@users.noreply.github.com>
2023-06-19 08:05:59 +0000

    Criação de dominio para o tipo CPF

--- app/models/bolsista.py
+++ app/models/bolsista.py
@@ -1,14 +1,38 @@
 from ..db import db
 from datetime import datetime
 from sqlalchemy.event import listens_for
-from datetime import date
+from sqlalchemy.orm import validates
+
+class CPFTipo(db.TypeDecorator):
+    impl = db.String(14)
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            # Remover caracteres não numéricos do CPF
+            value = ''.join(filter(str.isdigit, value))
+        return value
+
+    def process_result_value(self, value, dialect):
+        return value
+
+    @staticmethod
+    def validate_cpf(cpf):
+        # validação do cpf
+        return True
+
+    @validates('cpf')
+    def validate_cpf_column(self, key, cpf):
+        if not self.validate_cpf(cpf):
+            raise ValueError('CPF inválido')
+        return cpf
+    
 
 class Bolsista(db.Model):
     __tablename__ = 'bolsistas'
 
     id = db.Column(db.Integer,primary_key=True)
     nome = db.Column(db.String, nullable=False)
-    cpf = db.Column(db.String, nullable = False)
+    cpf = db.Column(CPFTipo, nullable = False)
     _data_nascimento = db.Column('data_nascimento', db.Date, nullable=False)
     foto_perfil_filekey = db.Column(db.String, nullable=True)
 
@@ -30,3 +54,5 @@ def name_to_upper(mapper, connection, target):
     target.nome = target.nome.upper()
 
 listens_for(Bolsista, 'before_insert')(name_to_upper)
+
+
